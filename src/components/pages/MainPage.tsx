428f904cff76e4d1dff88d736cfd40e412070c5d
Neto test <nikolajeliseev834@gmail.com>
2025-05-19 15:14:26 +0300

    Добавлена анимация загрузки главной страницы

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
 import { PostCard } from '../PostCard';
 import { usePosts } from '../../hooks/usePosts';
-import './pages.css'
+import './pages.css';
 
 export const MainPage = () => {
   const { posts, loading, error } = usePosts();
 
-  if (loading) return <div>Загрузка...</div>;
+  if (loading)
+    return (
+      <div className="typing-dots">
+        Загрузка <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </div>
+    );
   if (error) return <div>{error}</div>;
   return (
     <div className="make-post-container">
       <h1>Посты</h1>
-      <Link to="/posts/new" className="make-post__btn">Создать пост</Link>
+      <Link to="/posts/new" className="make-post__btn">
+        Создать пост
+      </Link>
       {posts.map(post => (
         <PostCard key={post.id} post={post} />
       ))}
